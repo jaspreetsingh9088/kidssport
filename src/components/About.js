00ae9ee3adb8130c5621e00512e083137561ccbd
unknown <guribwi123@gmail.com>
2024-12-21 14:00:33 +0530

    blog section updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,46 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import childplay from '../assets/images/childplay.png';
 import ticks from '../assets/images/ticks.svg';
 import circleiconframe from '../assets/images/circleiconframe.png';
 
 function About() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const section = document.querySelector('.kidzsport-about');
+      const sectionPosition = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      // Check if the section is in view
+      if (sectionPosition < windowHeight - 100) {
+        setIsVisible(true); // Add 'show' class
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on unmount
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <section className="about kidzsport-about">
+    <section className={`about kidzsport-about ${isVisible ? 'show' : ''}`}>
       <div className="container">
         <div className="row align-items-center row-reverse">
           <div className="col-lg-6">
-           
             <img src={childplay} alt="Childplay" className="child-play" />
           </div>
-          <div class="col-lg-6">
-             <div className='circle-framee'>
-            <img src={circleiconframe} alt='' className='circle-frame floating'/>
+          <div className="col-lg-6">
+            <div className='circle-framee'>
+              <img src={circleiconframe} alt='' className='circle-frame floating'/>
             </div>
-                    <div>
-                        <p class="about">ABOUT KIDZSPORT</p>
-                    </div>
-                    <div>
-                        <h2 class="Engaging">Engaging kids through fun</h2>
-                    </div>
-                    <div>
-                        <p class="interactive">interactive sports lessons and events, our company promotes healthy
-                            habits and teamwork. Join
-                            us on social media for tips, challenges, and exciting giveaways!</p>
-                    </div>
-                    <div class="d-flex gap-2 mb-2">
-                        <img src={ticks} alt="Childplay" className="check-icon" />
-                        <p class="Empowering">Empowering kids through dynamic activities</p>
-                    </div>
-                    <div class="d-flex gap-2 mb-2">
-                        <img src={ticks} alt="Childplay" className="check-icon" />
-                        <p class="Empowering">Dedicated team fosters a inclusive environment</p>
-                    </div>
-                    <div class="d-flex gap-2 mb-2">
-                        <img src={ticks} alt="Childplay" className="check-icon" />
-                        <p class="Empowering">Join us for a transformative experience</p>
-                    </div>
-                    <button class="Know">Know More</button>
-                </div>
+            <div>
+              <p className="about">ABOUT KIDZSPORT</p>
+            </div>
+            <div>
+              <h2 className="Engaging">Engaging kids through fun</h2>
+            </div>
+            <div>
+              <p className="interactive">
+                interactive sports lessons and events, our company promotes healthy
+                habits and teamwork. Join us on social media for tips, challenges, and exciting giveaways!
+              </p>
+            </div>
+            <div className="d-flex gap-2 mb-2">
+              <img src={ticks} alt="Childplay" className="check-icon" />
+              <p className="Empowering">Empowering kids through dynamic activities</p>
+            </div>
+            <div className="d-flex gap-2 mb-2">
+              <img src={ticks} alt="Childplay" className="check-icon" />
+              <p className="Empowering">Dedicated team fosters an inclusive environment</p>
+            </div>
+            <div className="d-flex gap-2 mb-2">
+              <img src={ticks} alt="Childplay" className="check-icon" />
+              <p className="Empowering">Join us for a transformative experience</p>
+            </div>
+            <button className="Know">Know More</button>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Latestblog.js
+++ src/components/Latestblog.js
@@ -1,16 +1,61 @@
 import React from 'react';
-
+import groupchild from '../assets/images/groupchild.png'
+import ludoplay from '../assets/images/ludoplay.png'
+import blocks from '../assets/images/blocks.png'
+import arrowright from '../assets/images/arrowright.png'
+import listdot from '../assets/images/listdot.png'
 
 function Latestblog() {
   return (
     <section className="latest-blog">
         <div className='container'>
         <h2 class="text-center text-center mt-0">Latest Blog</h2>
-        <p class="interactive interactive-one">Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.</p>
+        <p class="interactive interactive-one">Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. <br />Aliquam in hendrerit urna.</p>
+        <div className='row blog-gutter'>
             <div className='col-lg-4'>
-
+              <div className='box-blog'>
+              <img src={groupchild} alt="" className="blog-images" />
+              <div className='main-display-admin'>
+                <div className='d-flex gap-3'>
+                <div><p className='admin-name'><span className='color-dot'><img src={listdot} alt="" className="list-dot" /></span> Admin</p></div>
+                <div><p className='admin-name'><span className='color-dot'><img src={listdot} alt="" className="list-dot" /></span> Date : 22-11-2024</p></div>
+                </div>
+              </div>
+              <h5>Top 5 Benefits Bootcamp Building Confidence and Character</h5>
+              <button type="readmore" className='read-more'>Read More <span className='image-arrow-read'><img src={arrowright} alt="" className="blog-images-arrow" /></span></button>
+              </div>
+            </div>
+            <div className='col-lg-4'>
+              <div className='box-blog'>
+              <img src={ludoplay} alt="" className="blog-images" />
+              <div className='main-display-admin'>
+                <div className='d-flex gap-3'>
+                <div><p className='admin-name'><span className='color-dot'><img src={listdot} alt="" className="list-dot" /></span> Admin</p></div>
+                <div><p className='admin-name'><span className='color-dot'><img src={listdot} alt="" className="list-dot" /></span> Date : 22-11-2024</p></div>
+                </div>
+              </div>
+              <h5>Top 5 Benefits Bootcamp Building Confidence and Character</h5>
+              <button type="readmore" className='read-more'>Read More <span className='image-arrow-read'><img src={arrowright} alt="" className="blog-images-arrow" /></span></button>
+              </div>
+            </div>
+            <div className='col-lg-4'>
+              <div className='box-blog'>
+              <img src={blocks  } alt="" className="blog-images" />
+              <div className='main-display-admin'>
+                <div className='d-flex gap-3'>
+                <div><p className='admin-name'><span className='color-dot'><img src={listdot} alt="" className="list-dot" /></span> Admin</p></div>
+                <div><p className='admin-name'><span className='color-dot'><img src={listdot} alt="" className="list-dot" /></span> Date : 22-11-2024</p></div>
+                </div>
+              </div>
+              <h5>Top 5 Benefits Bootcamp Building Confidence and Character</h5>
+              <button type="readmore" className='read-more'>Read More <span className='image-arrow-read'><img src={arrowright} alt="" className="blog-images-arrow" /></span></button>
+              </div>
+            </div>
             </div>
         </div>
+        <script>
+          
+        </script>
 </section>
 
   );
